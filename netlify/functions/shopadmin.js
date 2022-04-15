c94e3867ea35bf80c6097357e4d362024bfe9dc6
dfragnito <dean@xornet.com>
2022-04-15 15:02:56 -0400

    Update shopadmin.js

--- netlify/functions/shopadmin.js
+++ netlify/functions/shopadmin.js
@@ -66,7 +66,7 @@ for (var j = 0; j < prods.length; j++){
 	 Object.entries(prods[j]).forEach(entry => {
 		  const [key, value] = entry;
 		  if(key== 'image'){ 
-		   document.write('<img src="http://sfsqlblogimages.s3.amazonaws.com/product-images/'+ value +'"/><br>'); 
+		   document.write('<img src="src="https://sfsqlblogimages.s3.amazonaws.com/product-images/'+ value +'"/><br>'); 
 			}else{
 			document.write(key + ": " +  value + "<br>");  
 			}
@@ -113,7 +113,7 @@ var t = 0;
 for (var j = 0; j < items1.length; j++){
       document.write("<tr>"); 
 		document.write("<td>" + items1[j]['no'] + "</td>"); 
-		document.write('<td><img src="http://sfsqlblogimages.s3.amazonaws.com/product-images/' + items1[j]['image'] + '"/><br>' + items1[j]['name'] + '</td>'); 
+		document.write('<td><img src="src="https://sfsqlblogimages.s3.amazonaws.com/product-images/' + items1[j]['image'] + '"/><br>' + items1[j]['name'] + '</td>'); 
 	   document.write("<td>" + items1[j]['code'] + "</td>"); 
 		document.write("<td>" + items1[j]['manname'] + "</td>");
 		document.write("<td>" + items1[j]['quantity'] + "</td>"); 
@@ -140,7 +140,7 @@ var t = 0;
 for (var j = 0; j < items2.length; j++){
       document.write("<tr>"); 
 		document.write("<td>" + items2[j]['no'] + "</td>"); 
-		document.write('<td><img src="http://sfsqlblogimages.s3.amazonaws.com/product-images/' + items2[j]['image'] + '"/><br>' + items2[j]['name'] + '</td>'); 
+		document.write('<td><img src="src="https://sfsqlblogimages.s3.amazonaws.com/product-images/' + items2[j]['image'] + '"/><br>' + items2[j]['name'] + '</td>'); 
 	   document.write("<td>" + items2[j]['code'] + "</td>"); 
 		document.write("<td>" + items2[j]['manname'] + "</td>");
 		document.write("<td>" + items2[j]['quantity'] + "</td>"); 
